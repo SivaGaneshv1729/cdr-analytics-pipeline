f74dfa519184d54884a950e64c0d5afce67c9404
Siva Ganesh <sivaganeshv1729@gmail.com>
2026-05-04 22:54:01 +0530

    Implement Apache Airflow DAGs for job orchestration

---
+++ dags/top_callers_dag.py
@@ -0,0 +1,20 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+default_args = {
+    'owner': 'airflow',
+    'start_date': datetime(2023, 1, 1),
+}
+
+with DAG(
+    'top_callers_by_spend_dag',
+    default_args=default_args,
+    schedule_interval=None,
+    catchup=False
+) as dag:
+
+    submit_job = BashOperator(
+        task_id='submit_spark_job',
+        bash_command='spark-submit --master spark://spark-master:7077 /jobs/top_callers.py --run_id {{ dag_run.conf.get("run_id", "default_run") }}'
+    )
